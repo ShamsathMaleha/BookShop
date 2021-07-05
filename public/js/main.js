532e481b4fd5f0e1738fbd73abc227b2eb74282b
ShamsathMaleha <17203011@iubat.edu>
2021-07-05 14:10:27 +0600

    update

---
+++ public/js/main.js
@@ -0,0 +1,19 @@
+// $('.autoplay').slick({
+//     slidesToShow: 3,
+//     slidesToScroll: 1,
+//     autoplay: true,
+//     autoplaySpeed: 2000,
+// });
+// 
+// $('.single-item').slick();
+
+
+$('.variable-width').slick({
+    dots: true,
+    infinite: true,
+    speed: 500,
+    fade: true,
+    cssEase: 'linear'
+});
+
+$('.single-item').slick();
